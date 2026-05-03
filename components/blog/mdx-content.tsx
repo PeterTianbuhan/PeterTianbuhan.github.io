487fd352d778552b888920609ce2a64d57083415
Letiancheng Lee <130898955+PeterTianbuhan@users.noreply.github.com>
2026-05-04 00:53:16 +0800

    PET-25: Publish HomePage generated bundle to GitHub Pages main (#5)

--- components/blog/mdx-content.tsx
+++
@@ -1,6 +0,0 @@
-import { MDXRemote } from "next-mdx-remote/rsc";
-import remarkGfm from "remark-gfm";
-
-export function MdxContent({ source }: { source: string }) {
-  return <MDXRemote options={{ mdxOptions: { remarkPlugins: [remarkGfm] } }} source={source} />;
-}
--- components/home/quote-panel.tsx
+++
@@ -1,9 +0,0 @@
-import { Panel } from "@/components/ui/panel";
-
-export function QuotePanel({ quote }: { quote: string }) {
-  return (
-    <Panel className="boot-in delay-4 rounded-[30px]">
-      <blockquote className="text-center text-lg leading-8 text-white/90 sm:text-xl">{quote}</blockquote>
-    </Panel>
-  );
-}
